04f345117abcfc989d4c3ce3c88c4d6c6293a691
Chris <chrisissifou@gmail.com>
2018-10-23 22:03:14 +0200

    itération 11

---
+++ symfony/src/OC/PlatformBundle/Entity/AdvertRepository.php
@@ -0,0 +1,58 @@
+<?php
+// src/OC/PlatformBundle/Entity/AdvertRepository.php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+
+
+class AdvertRepository extends EntityRepository
+{
+  public function getAdvertWithCategories(array $categoryNames)
+  {
+    $qb = $this->createQueryBuilder('a');
+
+    // On fait une jointure avec l'entité Category avec pour alias « c »
+    $qb
+      ->innerJoin('a.categories', 'c')
+      ->addSelect('c')
+    ;
+
+    // Puis on filtre sur le nom des catégories à l'aide d'un IN
+    $qb->where($qb->expr()->in('c.name', $categoryNames));
+    // La syntaxe du IN et d'autres expressions se trouve dans la documentation Doctrine
+
+    // Enfin, on retourne le résultat
+    return $qb
+      ->getQuery()
+      ->getResult()
+    ;
+  }
+
+
+
+
+  public function getApplicationsWithAdvert($limit)
+  {
+    $qb = $this->createQueryBuilder('a');
+
+    // On fait une jointure avec l'entité Advert avec pour alias « adv »
+    $qb
+      ->innerJoin('a.advert', 'adv')
+      ->addSelect('adv')
+    ;
+
+    // Puis on ne retourne que $limit résultats
+    $qb->setMaxResults($limit);
+
+    // Enfin, on retourne le résultat
+    return $qb
+      ->getQuery()
+      ->getResult()
+      ;
+  }
+
+  
+
+}
